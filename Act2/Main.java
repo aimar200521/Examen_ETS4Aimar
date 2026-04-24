763969b17ee2ccc3524b214173843f9bd317e083
Aimar Moreno Ramos <aimardayna@gmail.com>
2026-04-24 09:17:15 +0100

    Creada la clase Main

---
+++ Act2/Main.java
@@ -0,0 +1,12 @@
+package Act2;
+
+import java.util.Scanner;
+
+public class Main {
+    public void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        Cilindro cilindro1 = new Cilindro(2,3,4,6,7,8);
+        Cilindro cilindro2 = new Cilindro(2,2,8,7,5,4);
+        Cilindro cilindro3 = new Cilindro(3,5,9,2,8,1);
+    }
+}
